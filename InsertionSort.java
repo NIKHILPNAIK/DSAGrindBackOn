4fed2f015eeb280559fa28138589ba839b47a643
NIKHILPNAIK <141359284+NIKHILPNAIK@users.noreply.github.com>
2026-05-12 19:44:10 +0530

    Added insertion

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,7 +1,27 @@
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
-        
 
+        int [] arr = {12, 23, 9,51, 7};
+
+        int n = arr.length;
+
+        
+        for(int i=1; i<n; i++){
+            int temp = arr[i];
+ 
+            int j =i-1;
+
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] =temp;
+        }
+
+
+        System.out.println(Arrays.toString(arr));
         
     }
 }
