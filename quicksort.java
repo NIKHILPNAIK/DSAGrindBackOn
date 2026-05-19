f5a8efb0bfbce7da79c3b71e9a2d910f16c7b432
NIKHILPNAIK <141359284+NIKHILPNAIK@users.noreply.github.com>
2026-05-19 17:59:07 +0530

    added the quicksort

---
+++ quicksort.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class quicksort {
+    public static void  sort(int []arr , int low, int high){
+
+        if(low>=high){
+            return;
+
+        }
+
+        int s = low;
+        int e = high;
+        int m = s+(e-s)/2;
+        int pivot = arr[m];
+
+        while(s<=e){
+
+            while(arr[s]< pivot){
+                s++;
+            }
+            while (arr[e]>pivot) {
+                e--;
+            }
+
+            if(s<=e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+
+            }
+        }
+        sort(arr, low, e);
+        sort(arr, s, high);
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        int [] arr = {3, 5, 4, 6, 7};
+
+        int n = arr.length;
+
+        sort(arr, 0, n-1);
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
